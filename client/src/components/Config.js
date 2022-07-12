9238c44404b2fc5eadf01e3a64036cf238f199de
kos9310 <kos9310@gmail.com>
2022-07-12 11:21:53 +0900

    landingPage

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '170f71f8fbd6b5a42034bb463a6b0f22';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
